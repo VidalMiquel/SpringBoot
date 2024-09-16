4563caed569042c67633f94ff6c59e9e479e0fbc
Miquel Vidal <mvidalc@riu.com>
2024-09-16 15:12:59 +0200

    restrucure projects; RIU mode

--- demo/src/main/java/com/example/demo/Presentation/Controller/UserController.java
+++ demo/src/main/java/com/example/demo/Presentation/Controller/UserController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.Presentation.Controller;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.User;
-import com.example.demo.service.UserService;
+import com.example.demo.Application.service.UserService;
+import com.example.demo.Domain.model.User;
 
 @RestController
 @RequestMapping("/users")
